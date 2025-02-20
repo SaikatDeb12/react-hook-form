b68d965f272a40246fbece0892fd6ae8990d5d5a
SaikatDeb12 <saikat.rcld@gmail.com>
2025-02-20 12:23:30 +0530

    using zod

--- src/Form2.jsx
+++ src/Form2.jsx
@@ -1,22 +1,44 @@
-import React from "react";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { object, z } from "zod";
+import { useEffect } from "react";
+
+const onSubmit = (e) => {
+  console.log(e);
+};
+
+const schema = z.object({
+  name: z.string().min(1, "required"),
+  address: z.string().min(1, "required"),
+  phone: z.string().length(10, "Phn no. should be of 10 digits"),
+  email: z.string().email("invalid"),
+});
 
 const Form2 = () => {
-  const { register, handleSubmit } = useForm();
-
-  const funcSubmit = (data) => {
-    console.log(data);
-  };
-
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+  useEffect(() => {
+    console.log(errors);
+  }, [errors]);
   return (
     <div>
-      <form onSubmit={handleSubmit(funcSubmit)}>
-        <input type="text" placeholder="Name" {...register("name")} />
-        <input type="text" placeholder="Address" {...register("add")} />
-        <button type="submit">Submit</button>
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" placeholder="Name: " {...register("name")} />
+        {errors.name && <p>{errors.name.message}</p>}
+        <input type="text" placeholder="Address: " {...register("address")} />
+        {errors.address && <p>{errors.address.message}</p>}
+        <input type="number" placeholder="Phone: " {...register("phone")} />
+        {errors.phone && <p>{errors.phone.message}</p>}
+        <input type="text" placeholder="email: " {...register("email")} />
+        {errors.email && <p>{errors.email.message}</p>}
+        <input type="submit" />
       </form>
     </div>
   );
 };
-
 export default Form2;
